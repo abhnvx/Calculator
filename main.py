3dd478c7f9d718dbd7d020b3e4ebce8acecb1f31
Abhinav Gupta <abhinavgupta@Abhinavs-MacBook.local>
2020-08-14 11:09:02 +0530

    Initializing - Abhinav

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+a = int(input("Enter first operand : "))
+b = int(input("Enter second operand : "))
+
+print("----------CALCULATOR----------")
+print("1.Add[a+b]")
+print("2.Subtract[a-b]")
+print("3.Multiply[a*b]")
+print("4.Divide[a/b]")
+print("5.Power[a^b]")
+print("6.Mod[a%b]")
+
+op = int(input("Enter operation [1-6]"))
+
+if op == 1:
+
+elif op == 2:
+
+elif op == 3:
+
+elif op == 4:
+
+elif op == 5:
+
+elif op == 6:
+
+    
